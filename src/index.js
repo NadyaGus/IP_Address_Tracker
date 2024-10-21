071cbc70447988ac24f05b213a5e38bb2ee20fa6
NadyaGus <ftotkexit@gmail.com>
2024-10-21 15:17:34 +0300

    feat: set location data in header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+import { validateIp } from "./helpers";
+
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('.search-bar__btn');
-const apiKey = 'at_szVmSzB7IDvSkZ3IM29T3IlSn2Rhm'
+const apiKey = 'at_szVmSzB7IDvSkZ3IM29T3IlSn2Rhm';
+
+const ipInfo = document.querySelector('#ip');
+const locationInfo = document.querySelector('#location');
+const timezoneInfo = document.querySelector('#timezone');
+const ispInfo = document.querySelector('#isp');
 
 btn.addEventListener('click', () => {
     getData();
@@ -11,15 +18,24 @@ ipInput.addEventListener('keydown', (e) => {
 })
 
 function getData() {
-    fetch(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${ipInput.value}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
+    if (validateIp(ipInput.value)) {
+        fetch(`https://geo.ipify.org/api/v2/country?apiKey=${apiKey}&ipAddress=${ipInput.value}`)
+        .then(response => response.json())
+        .then(data => {
+            setInfo(data)
+        })
+    }
 }
 
 function handleKey(e) {
     if (e.key === 'Enter') {
         getData();
     }
+}
+
+function setInfo(mapData) {
+    ipInfo.textContent = mapData.ip;
+    locationInfo.textContent = `${mapData.location.country} ${mapData.location.region}`;
+    timezoneInfo.textContent = mapData.location.timezone;
+    ispInfo.textContent = mapData.isp;
 }
